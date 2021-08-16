1b633b05c8c21884887b85266c6427b3118dc84a
Daniel McCloy <dan@mccloy.info>
2021-08-16 14:50:16 -0500

    proper surrogate, merge inferiorfrontal labels, label colors

--- analysis/env_corr.py
+++ analysis/env_corr.py
@@ -12,6 +12,8 @@ import os
 from collections import Counter
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import to_rgba
 import mne
 from mne.minimum_norm import apply_inverse_epochs
 import mnefun
@@ -35,9 +37,61 @@ mnefun_params_fname = os.path.join('..', 'preprocessing', 'mnefun_params.yaml')
 mnefun_params = mnefun.read_params(mnefun_params_fname)
 lp_cut = int(mnefun_params.lp_cut)
 
+# colors
+label_colors = {
+    'caudalanteriorcingulate': '#D1BBD7',  # purple, light
+    'inferiorparietal': '#4EB265',  # green, dark
+    'inferiortemporal': '#AE76A3',  # purple
+    'insula': '#CAE0AB',  # green, light
+    'lateralorbitofrontal': '#882E72',  # purple, dark
+    'medialorbitofrontal': '#F7F056',  # yellow
+    'middletemporal': '#1965B0',  # blue, dark
+    'posteriorcingulate': '#F4A736',  # orange, light
+    'rostralanteriorcingulate': '#5289C7',  # blue
+    'superiorparietal': '#E8601C',  # orange
+    'superiortemporal': '#7BAFDE',  # blue, light
+    'temporalpole': '#DC050C',  # red
+    'inferiorfrontal': '#90C987',  # green pars{opercular|orbital|triangular}is
+}
+
+# '#D1BBD7',  # purple, light
+# '#AE76A3',  # purple
+# '#882E72',  # purple, dark
+# '#1965B0',  # blue, dark
+# '#5289C7',  # blue
+# '#7BAFDE',  # blue, light
+# '#4EB265',  # green, dark
+# '#90C987',  # green
+# '#CAE0AB',  # green, light
+# '#F7F056',  # yellow
+# '#F4A736',  # orange, light
+# '#E8601C',  # orange
+# '#DC050C',  # red
+
 # load ROIs
 rois = load_params(os.path.join(param_dir, 'rois.yaml'))
+rois_to_merge = ('parsorbitalis', 'parsopercularis', 'parstriangularis')
+rois = set(rois) - set(rois_to_merge)
 roi_regexp = '|'.join(rois)
+# get regular labels
+labels = mne.read_labels_from_annot(
+    'ANTS6-0Months3T', parc='aparc', subjects_dir=subjects_dir,
+    regexp=roi_regexp)
+# get merged labels
+for h in ('lh', 'rh'):
+    _regexp = '|'.join((f'{roi}-{h}' for roi in rois_to_merge))
+    _labels = mne.read_labels_from_annot(
+        'ANTS6-0Months3T', parc='aparc', subjects_dir=subjects_dir,
+        regexp=_regexp)
+    merged_label = sum(_labels[1:], _labels[0])
+    labels.append(merged_label)
+# set label colors
+for label in labels:
+    if '+' in label.name:  # merged label
+        label.color = label_colors['inferiorfrontal']
+    else:
+        label.color = label_colors[label.name.rsplit('-', maxsplit=1)[0]]
+
 
 # find out how many events we can realistically keep
 n_good_epochs = dict(attend=dict(), ignore=dict())
@@ -67,11 +121,6 @@ for subj in subjects:
     pick_ori = inverse_params['estimate_type']
     stcs = apply_inverse_epochs(epochs, inverse_operator, lambda2, method,
                                 pick_ori=pick_ori, return_generator=True)
-    # get 'aparc' labels
-    # (don't pass experiment-specific subjects_dir, use system-default one)
-    labels = mne.read_labels_from_annot(
-        'sample', parc='aparc', subjects_dir=None, regexp=roi_regexp)
-    label_colors = [label.color for label in labels]
     # get average signal in each label (mean_flip reduces signal cancellation)
     src = inverse_operator['src']
     label_timeseries = mne.extract_label_time_course(
@@ -82,11 +131,7 @@ for subj in subjects:
     fname = f'{subj}-theta-band-envelope-correlation.npy'
     np.save(os.path.join(corr_dir, fname), corr)
 
-
     # XXX TODO RESUME HERE
     fig = plt.figure(figsize=(6, 6))
-# let's plot this matrix
-fig, ax = plt.subplots(figsize=(4, 4))
-ax.imshow(corr, cmap='viridis', clim=np.percentile(corr, [5, 95]))
-fig.tight_layout()
-del epochs, stcs, label_ts
+    ax = fig.add_subplot()
+    ax.imshow(corr, cmap='magma', clim=np.percentile(corr, [5, 95]))
