8b4e9da78044ee01c159715e9fc771d4ba37433b
Daniel McCloy <dan@mccloy.info>
2021-08-24 15:37:27 -0500

    fix: write trans; better user interaction

--- preprocessing/run_coreg.py
+++ preprocessing/run_coreg.py
@@ -23,22 +23,25 @@ scaling_already_done = True
 # subject's digitization points. Then scale the MRI and make the BEM solution.
 for subject in subjects:
     if scaling_already_done:
-        continue
+        break
     print('## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ##')
     print(f'##               NOW SCALING SUBJECT {subject}                ##')
     print('## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ## # ##')
-    raw_fname = os.path.join(data_root, subject, 'raw_fif',
-                             f'{subject}_raw.fif')
+    this_subj_dir = os.path.join(data_root, subject, 'raw_fif')
+    raw_fname = os.path.join(this_subj_dir, f'{subject}_raw.fif')
     info = mne.io.read_info(raw_fname)
     coreg = mne.coreg.Coregistration(
         info, subject=surrogate, subjects_dir=subjects_dir)
     coreg.set_scale_mode('3-axis')
     coreg.set_fid_match('matched')
     coreg.fit_icp(n_iterations=30)
+    # save the trans file
+    trans_fname = os.path.join(this_subj_dir, f'{subject}-trans.fif')
+    mne.write_trans(trans_fname, coreg.trans)
     # this step takes a while
     mne.scale_mri(subject_from=surrogate, subject_to=subject,
-                  scale=coreg.scale, overwrite=True, subjects_dir=subjects_dir,
-                  verbose=True)
+                  scale=coreg.scale, overwrite=False,
+                  subjects_dir=subjects_dir, verbose=True)
     # make BEM solution
     bem_in = os.path.join(
         subjects_dir, subject, 'bem', f'{subject}-5120-5120-5120-bem.fif')
@@ -54,17 +57,20 @@ for subject in subjects:
     trans_fname = os.path.join(this_subj_dir, f'{subject}-trans.fif')
     info = mne.io.read_info(raw_fname)
     trans = mne.read_trans(trans_fname)
-    print(subject)
     mne.viz.plot_alignment(
         info=info, trans=trans, subject=subject, subjects_dir=subjects_dir,
         surfaces=dict(head=0.9), dig=True, mri_fiducials=True, meg=False)
-    # run coregistration GUI to do it manually and compare
-    mne.gui.coregistration(inst=raw_fname,
-                           subject=surrogate,
-                           subjects_dir=subjects_dir,
-                           guess_mri_subject=False,
-                           mark_inside=True)
-    response = input(f'You just saw {subject}; press <ENTER> to continue or X '
-                     'to quit\n')
+    # user interaction
+    spaces = ' ' * (len(subject) + 14)
+    response = input(f'Now viewing {subject}; press <ENTER> to continue;\n'
+                     f'{spaces}press C <ENTER> to run manual coreg\n'
+                     f'{spaces}press X <ENTER> to quit\n')
     if response.lower().startswith('x'):
         break
+    elif response.lower().startswith('c'):
+        # run coregistration GUI to do it manually and compare
+        mne.gui.coregistration(inst=raw_fname,
+                               subject=surrogate,
+                               subjects_dir=subjects_dir,
+                               guess_mri_subject=False,
+                               mark_inside=True)
