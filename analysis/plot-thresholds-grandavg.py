845bfea468f7dd9f2005666266232a944c471fbd
Daniel McCloy <dan@mccloy.info>
2021-10-08 16:18:44 -0500

    loop over parcellations in threshold plotter

--- analysis/plot-thresholds-grandavg.py
+++ analysis/plot-thresholds-grandavg.py
@@ -10,6 +10,7 @@ license: MIT
 import os
 import numpy as np
 import mne
+from mne.minimum_norm import read_inverse_operator
 import mne_connectivity
 import mnefun
 from f2f_helpers import load_paths, load_subjects, load_params, get_skip_regexp
@@ -46,56 +47,57 @@ mnefun_params_fname = os.path.join('..', 'preprocessing', 'mnefun_params.yaml')
 mnefun_params = mnefun.read_params(mnefun_params_fname)
 lp_cut = int(mnefun_params.lp_cut)
 
-# load labels
+# label regexp ignores "???" & "unknown" by default
 regexp = get_skip_regexp()
-labels = mne.read_labels_from_annot('fsaverage', 'aparc_sub', regexp=regexp,
-                                    subjects_dir=None)
+parcellations = ('aparc', 'aparc_sub', 'HCPMMP1_combined', 'HCPMMP1')
 
-for threshold_prop in threshold_props:
-    grandavg_stc = None
-    for subj in subjects:
-        # morph labels
-        this_labels = mne.morph_labels(
-            labels, subject_to=subj, subject_from='fsaverage',
-            subjects_dir=subjects_dir)
-        label_names = [label.name for label in this_labels]
-        # load source space (from inverse)
-        inv_fnames = dict(
-            erm=f'{subj}-meg-erm{orientation_constraint}-inv.fif',
-            baseline=f'{subj}-{lp_cut}-sss-meg{orientation_constraint}-inv.fif'
-        )
-        inv_fname = inv_fnames[cov_type]
-        inv_fpath = os.path.join(data_root, subj, 'inverse', inv_fname)
-        source_space = mne.minimum_norm.read_inverse_operator(inv_fpath)['src']
-        # load connectivity
-        slug = get_slug(subj, freq_band, condition)
-        conn_fname = (f'{slug}-envelope-correlation.nc')
-        conn_fpath = os.path.join(conn_dir, conn_fname)
-        conn = mne_connectivity.read_connectivity(conn_fpath)
-        full_degree = mne_connectivity.degree(conn, threshold_prop)
-        # convert to STC
-        stc = mne.labels_to_stc(this_labels, full_degree)
-        stc = stc.in_label(
-            mne.Label(source_space[0]['vertno'], hemi='lh') +
-            mne.Label(source_space[1]['vertno'], hemi='rh'))
-        # aggregate
-        if grandavg_stc is None:
-            grandavg_stc = stc
-        else:
-            grandavg_stc.data += stc.data
-        del conn, full_degree
-    grandavg_stc.data /= len(subjects)
 
-    # plot grand average STC
-    brain = grandavg_stc.plot(
-        clim=dict(kind='percent', lims=[75, 85, 95]),
-        colormap='plasma',
-        subjects_dir=subjects_dir,
-        views=['lateral', 'medial', 'dorsal'], hemi='split',
-        smoothing_steps='nearest', time_label=f'{freq_band} band',
-        size=(900, 1200), background='w', title=slug)
-    # save
-    slug = get_slug('grandavg', freq_band, condition)
-    img_fname = f'{slug}-degree-thresh_{threshold_prop:.02f}.png'
-    brain.save_image(os.path.join(plot_dir, img_fname))
-    brain.close()
+for parcellation in parcellations:
+    for threshold_prop in threshold_props:
+        grandavg_stc = None
+        for subj in subjects:
+            # load labels
+            labels = mne.read_labels_from_annot(
+                subj, parcellation, regexp=regexp, subjects_dir=subjects_dir)
+            label_names = [label.name for label in labels]
+            # load source space (from inverse)
+            inv_fnames = dict(
+                erm=f'{subj}-meg-erm{orientation_constraint}-inv.fif',
+                baseline=(f'{subj}-{lp_cut}-sss-meg{orientation_constraint}'
+                          '-inv.fif')
+            )
+            inv_fname = inv_fnames[cov_type]
+            inv_fpath = os.path.join(data_root, subj, 'inverse', inv_fname)
+            source_space = read_inverse_operator(inv_fpath)['src']
+            # load connectivity
+            slug = get_slug(subj, freq_band, condition)
+            conn_fname = (f'{parcellation}-{slug}-envelope-correlation.nc')
+            conn_fpath = os.path.join(conn_dir, conn_fname)
+            conn = mne_connectivity.read_connectivity(conn_fpath)
+            full_degree = mne_connectivity.degree(conn, threshold_prop)
+            # convert to STC
+            stc = mne.labels_to_stc(labels, full_degree)
+            stc = stc.in_label(
+                mne.Label(source_space[0]['vertno'], hemi='lh') +
+                mne.Label(source_space[1]['vertno'], hemi='rh'))
+            # aggregate
+            if grandavg_stc is None:
+                grandavg_stc = stc
+            else:
+                grandavg_stc.data += stc.data
+            del conn, full_degree
+        grandavg_stc.data /= len(subjects)
+
+        # plot grand average STC
+        brain = grandavg_stc.plot(
+            clim=dict(kind='percent', lims=[75, 85, 95]),
+            colormap='plasma',
+            subjects_dir=subjects_dir,
+            views=['lateral', 'medial', 'dorsal'], hemi='split',
+            smoothing_steps='nearest', time_label=f'{freq_band} band',
+            size=(900, 1200), background='w', title=slug)
+        # save
+        slug = get_slug('grandavg', freq_band, condition)
+        img_fname = f'{slug}-degree-thresh_{threshold_prop:.02f}.png'
+        brain.save_image(os.path.join(plot_dir, parcellation, img_fname))
+        brain.close()
