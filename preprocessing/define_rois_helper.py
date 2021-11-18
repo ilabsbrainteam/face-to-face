d7379cf3550952abd7cd34b1e5238b6d368ff92a
Daniel McCloy <dan@mccloy.info>
2021-11-18 16:47:00 -0600

    tweak ROI defs (forgot lTPJ & rVFC)

--- preprocessing/define_rois_helper.py
+++ preprocessing/define_rois_helper.py
@@ -28,14 +28,17 @@ borders = (False,) * 8 + (True,) * 8
 raise RuntimeError()
 # convenience variables. Run blocks below interactively as needed after
 # changing these variables.
-bilateral = 'superiortemporal'
+bilateral = 'supramarginal'
 left_hemi = 'parsopercularis'
 
 # bilateral
 for n, (col, bord) in enumerate(zip(colors, borders), start=1):
-    for hemi in ('lh', 'rh'):
-        brain.add_label(label_dict[f'{bilateral}_{n}-{hemi}'],
-                        color=col, borders=bord, alpha=0.5)
+    for hemi in ('rh', 'lh'):
+        try:
+            brain.add_label(label_dict[f'{bilateral}_{n}-{hemi}'],
+                            color=col, borders=bord, alpha=0.5)
+        except KeyError:
+            continue
 
 # left hemi only
 for n, (col, bord) in enumerate(zip(colors, borders), start=1):
