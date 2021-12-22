c8c5f7ded3a1f94c6750e6eae99966d225589ab9
Daniel McCloy <dan@mccloy.info>
2021-12-21 18:09:49 -0600

    update custom parcellation script to handle multiple custom models

--- preprocessing/copy_custom_parcellation.py
+++ preprocessing/copy_custom_parcellation.py
@@ -11,6 +11,9 @@ import os
 import mne
 from f2f_helpers import load_paths, load_params, load_subjects
 
+# flags
+save_individual_labels = False
+
 # config paths
 data_root, subjects_dir, results_dir = load_paths()
 param_dir = os.path.join('..', 'params')
@@ -19,24 +22,28 @@ param_dir = os.path.join('..', 'params')
 subjects = load_subjects()
 surrogate = load_params(os.path.join(param_dir, 'surrogate.yaml'))
 
-# load the annotation from surrogate
-labels = mne.read_labels_from_annot(
-    surrogate, 'hickok_corbetta', subjects_dir=subjects_dir)
-
-# copy to each subj
-for subj in subjects:
-    # morph labels
-    new_labels = mne.morph_labels(
-        labels, subject_to=subj, subject_from=surrogate,
-        subjects_dir=subjects_dir)
-    # rename labels
-    for label in new_labels:
-        label.name = f'f2f-{label.name}'
-    mne.write_labels_to_annot(
-        labels=new_labels, subject=subj, parc='hickok_corbetta',
-        subjects_dir=subjects_dir, overwrite=True)
-    # save labels
-    for label in new_labels:
-        fname = f'{label.hemi}.{label.name.rsplit("-", maxsplit=1)[0]}.label'
-        fpath = os.path.join(subjects_dir, subj, 'label', fname)
-        mne.write_label(fpath, label)
+# loop over ROI definitions / models
+roi_dict = load_params(os.path.join(param_dir, 'rois.yaml'))
+for parcellation in roi_dict:
+    # load the annotation from surrogate
+    labels = mne.read_labels_from_annot(
+        surrogate, parcellation, subjects_dir=subjects_dir)
+    # copy to each subj
+    for subj in subjects:
+        # morph labels
+        new_labels = mne.morph_labels(
+            labels, subject_to=subj, subject_from=surrogate,
+            subjects_dir=subjects_dir)
+        # rename labels
+        for label in new_labels:
+            label.name = f'{parcellation}-{label.name}'
+        mne.write_labels_to_annot(
+            labels=new_labels, subject=subj, parc=parcellation,
+            subjects_dir=subjects_dir, overwrite=True)
+        # save labels
+        if save_individual_labels:
+            for label in new_labels:
+                fname = (f'{label.hemi}.'
+                         f'{label.name.rsplit("-", maxsplit=1)[0]}.label')
+                fpath = os.path.join(subjects_dir, subj, 'label', fname)
+                mne.write_label(fpath, label)
